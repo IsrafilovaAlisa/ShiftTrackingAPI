e6422a445f2cca36407f1363fb5b55c909768e8b
A_D_M_I_N <A_D_M_I_N@DESKTOP-HO6124K>
2025-08-18 17:31:06 +0500

    Подготовка контроллеров: контроллер для управления сотрудников, контроллер для управления временем работы сотрудников

---
+++ ShiftTrackingAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShiftTrackingAPI.Helpers.SQL;
+
+namespace ShiftTrackingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController: ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ ShiftTrackingAPI/Controllers/TimeTrackerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShiftTrackingAPI.Helpers.SQL;
+
+namespace ShiftTrackingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeTrackerController: ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TimeTrackerController(AppDbContext context) 
+        {
+            _context = context;
+        }
+    }
+}
