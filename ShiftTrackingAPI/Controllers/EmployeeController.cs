3635ac0fe61ce0192f516ac51085c3b13e502b6b
A_D_M_I_N <A_D_M_I_N@DESKTOP-HO6124K>
2025-08-18 22:05:12 +0500

    Делаем гет запрос на получение сотрудников с необязательным параметром профессии

--- ShiftTrackingAPI/Controllers/EmployeeController.cs
+++ ShiftTrackingAPI/Controllers/EmployeeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShiftTrackingAPI.Helpers.SQL;
-
+using ShiftTrackingAPI.Helpers.SQL.Queries;
+using ShiftTrackingAPI.Models;
+using System;
+using System.Threading.Tasks;
+#nullable enable
 namespace ShiftTrackingAPI.Controllers
 {
     [ApiController]
@@ -11,6 +15,19 @@ namespace ShiftTrackingAPI.Controllers
         public EmployeeController(AppDbContext context)
         {
             _context = context;
+        }
+        [HttpGet("GetEmployee")]
+        public async Task<IActionResult> GetEmployee([FromServices] AppDbContext context,  [FromQuery]Position? position)
+        {
+            if(position != null)
+            {
+                if (!Enum.IsDefined(typeof(Position), position))
+                {
+                    return BadRequest(new { error = "Нет такой должности" });
+                }
+            }
+            var result = await EmployeeQueries.GetEmployee(context, position);
+            return Ok(new { data = result });
         }
     }
 }
---
+++ ShiftTrackingAPI/Helpers/SQL/Queries/EmployeeQueries.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftTrackingAPI.Models;
+using ShiftTrackingAPI.Models.DTO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShiftTrackingAPI.Helpers.SQL.Queries
+{
+    public static class EmployeeQueries
+    {
+        public static async Task<List<EmployeeDTO>> GetEmployee(AppDbContext context, Position? position)
+        {
+            var query = context.employees.AsNoTracking();
+            return await query
+                .Where(e => position == null || e.Position == position)
+                .Select(e => new EmployeeDTO
+                {
+                    Id = e.Id,
+                    LastName = e.LastName,
+                    FirstName = e.FirstName,
+                    MiddleName = e.MiddleName,
+                    Position = e.Position
+                }).ToListAsync();
+        } 
+    }
+}
