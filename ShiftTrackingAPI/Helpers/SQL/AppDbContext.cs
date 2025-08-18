ae269b272486bf1e91fab39fdca44b5c69fae347
A_D_M_I_N <A_D_M_I_N@DESKTOP-HO6124K>
2025-08-18 17:20:20 +0500

    Создание и Регистрация бд контекста

---
+++ ShiftTrackingAPI/Helpers/SQL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftTrackingAPI.Models;
+
+namespace ShiftTrackingAPI.Helpers.SQL
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Shift> shifts { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- ShiftTrackingAPI/Startup.cs
+++ ShiftTrackingAPI/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ShiftTrackingAPI.Helpers.SQL;
 
 namespace ShiftTrackingAPI
 {
@@ -28,6 +23,7 @@ namespace ShiftTrackingAPI
         {
 
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=ShiftTracking.db"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShiftTrackingAPI", Version = "v1" });
