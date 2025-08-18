f9d75e5c1cdd9585c8439285ac1e3ab6f787bf19
A_D_M_I_N <A_D_M_I_N@DESKTOP-HO6124K>
2025-08-18 22:04:28 +0500

    Создаем дтошки для работы с клиентом

---
+++ ShiftTrackingAPI/Models/DTO/EmployeeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ShiftTrackingAPI.Models.DTO
+#nullable enable
+{
+    public class EmployeeDTO
+    {
+        public long Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public Position? Position { get; set; }
+    }
+}
---
+++ ShiftTrackingAPI/Models/DTO/ShiftDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShiftTrackingAPI.Models.DTO
+{
+    public class ShiftDTO
+    {
+        public long Id { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public double WorkTime { get; set; }
+    }
+}
