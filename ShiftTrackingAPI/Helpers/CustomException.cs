12d67c825ac2114b9fedb26fdcbedd2c22d3068c
A_D_M_I_N <A_D_M_I_N@DESKTOP-HO6124K>
2025-08-19 19:31:26 +0500

    сделаны кастомные исключения

---
+++ ShiftTrackingAPI/Helpers/CustomException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShiftTrackingAPI.Helpers
+{
+    public class CustomException: Exception
+    {
+        public ErrorType Type { get; }
+        public CustomException(ErrorType type) : base(type.ToString()) { Type = type; }
+    }
+}
---
+++ ShiftTrackingAPI/Helpers/ErrorType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ShiftTrackingAPI.Helpers
+{
+    public enum ErrorType
+    {
+        /// <summary>нет ошибки</summary>
+        None = 0,
+        /// <summary>не найдено </summary>
+        NotFound = 1,
+        /// <summary>  введены неверные данные </summary>
+        DataIncorrect = 2,
+        /// <summary> работа с ошибками даты</summary>
+        DateIncorrect = 3,
+    }
+}
