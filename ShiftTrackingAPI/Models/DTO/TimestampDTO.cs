a7eef33efd949b0ae351163c3ac9f58d50a7f3fb
Alisa <gggg_gggg_21@mail.ru>
2025-08-29 20:12:50 +0500

    Добавлен атрибут валидации обязательного поля в вспомогательную дтоху со временем смены

--- ShiftTrackingAPI/Models/DTO/TimestampDTO.cs
+++ ShiftTrackingAPI/Models/DTO/TimestampDTO.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShiftTrackingAPI.Models.DTO
 {
     public class TimestampDTO
     {
+        [Required(ErrorMessage = "Укажите время")]
         public DateTime Timestamp { get; set; }
     }
 }
