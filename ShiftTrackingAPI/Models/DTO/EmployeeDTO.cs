2380f93cebaa70e40ce1be0771c5e8fb307b6907
Alisa <gggg_gggg_21@mail.ru>
2025-08-29 19:54:30 +0500

    Добавлены атрибуты валидации в дтохи сотрудников, смен

--- ShiftTrackingAPI/Models/DTO/EmployeeDTO.cs
+++ ShiftTrackingAPI/Models/DTO/EmployeeDTO.cs
@@ -1,12 +1,17 @@
-﻿namespace ShiftTrackingAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
 #nullable enable
+namespace ShiftTrackingAPI.Models.DTO
 {
     public class EmployeeDTO
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Фамилия обязательна")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Имя обязательно")]
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
+        [Required(ErrorMessage = "Должность обязательна")]
+        [EnumDataType(typeof(Position), ErrorMessage = "Несуществует указанной должности")]
         public Position Position { get; set; }
     }
 }
--- ShiftTrackingAPI/Models/DTO/ShiftDTO.cs
+++ ShiftTrackingAPI/Models/DTO/ShiftDTO.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShiftTrackingAPI.Models.DTO
 {
     public class ShiftDTO
     {
         public long Id { get; set; }
+        [Required(ErrorMessage = "Сотрудника нет")]
         public long EmployeeId { get; set; }
+        [Required(ErrorMessage = "Укажите время начала смены")]
         public DateTime? From { get; set; }
+        [Required(ErrorMessage = "Укажите время конца смены")]
         public DateTime? To { get; set; }
-        public double? WorkTime { get; set; }
+        public double? WorkTimeHours { get; set; }
     }
 }
