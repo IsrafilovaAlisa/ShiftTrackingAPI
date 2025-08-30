647cf9b4841c671c930453a72564ee969cf354b9
Alisa <gggg_gggg_21@mail.ru>
2025-08-30 12:29:36 +0500

    убрана явная валидация в контроллере при создании сотрудника, добавлен атрибут на статус ответа для док-ии апи

--- ShiftTrackingAPI/Controllers/EmployeeController.cs
+++ ShiftTrackingAPI/Controllers/EmployeeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using ShiftTrackingAPI.Helpers.SQL;
 using ShiftTrackingAPI.Helpers.SQL.Queries;
 using ShiftTrackingAPI.Models;
 using ShiftTrackingAPI.Models.DTO;
+using ShiftTrackingAPI.Models.DTO.Response;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -37,12 +39,10 @@ namespace ShiftTrackingAPI.Controllers
             return Ok(Enum.GetNames(typeof(Position)));
         }
         [HttpPost("CreateEmployee")]
-        public async Task<IActionResult> CreateEmployee([FromServices] AppDbContext context, [FromBody] EmployeeDTO obj)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EmployeeResponseDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse))]
+        public async Task<ActionResult<EmployeeResponseDTO>> CreateEmployee([FromServices] AppDbContext context, [FromBody] EmployeeDTO obj)
         {
-            if (string.IsNullOrWhiteSpace(obj.LastName) || string.IsNullOrWhiteSpace(obj.FirstName) || !Enum.IsDefined(typeof(Position), obj.Position))
-            {
-                return BadRequest(new { error = "Неверно введены данные" });
-            }
             return Ok(new { data = await EmployeeQueries.CreateEmployee(context, obj) });
         }
         [HttpPut("UpdateEmployee")]
